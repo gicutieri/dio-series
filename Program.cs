3424aa5aa6e10cf0888465bfd39235da9c5f5c76
gicutieri <gi_cutieri@hotmail.com>
2021-07-27 22:46:51 -0300

    inicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dio_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ classes/Serie.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace dio_series
+{
+    public class Serie : EntidadeBase
+    {
+        //atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        //métodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano){
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString(){
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano: " + this.Ano;
+            return retorno;
+        }
+
+        public string retornaTitulo(){
+            return this.Titulo;
+        }
+
+        public int retornaId(){
+            return this.Id;
+        }
+    }
+}
